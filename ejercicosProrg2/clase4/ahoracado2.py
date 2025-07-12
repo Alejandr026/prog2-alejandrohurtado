fddf68e68c986e872fc59a4874e7e1c2be638507
AlE <alejandrohurtadorodas@gmail.com>
2025-07-11 21:52:36 -0400

    Nueva carpeta docente

--- ejercicosProrg2/clase4/ahoracado2.py
+++ ejercicosProrg2/clase4/ahoracado2.py
@@ -6,17 +6,23 @@ print("\nCrea una lista secreta de numeros y que alguien intente adivinarlos")
 print("\nCuando quieras intentar adivinar la lista, escribe 'adivinar'")
 print("\n------------------------------------------------------------")
 
+#Definimos nuestras listas 
 lis_secret = []
 list_pista = []
+#Primer bloque donde creamos nuestras lista y definimos su tamaño
 while True:
     try:
         log_lista = int(input("Ingrese el tamaño de la lista secreta (no mayor a 10): "))
         if log_lista <= 0 or log_lista > 10:
             print("❌ Tamaño no válido. Intenta de nuevo.")
         else:
+            #Un bucle que recore el tamaño de elementos de la lista y pide ingresas nuestros valores a la lista
             for i in range(log_lista):
+                #se usa el getpass para que no se vea en la consola a la hora de ingresar los numeros
                 numero = int(getpass.getpass(f"Ingrese el número #{i + 1}: "))
+                #.append para añadir los valores a nuestra lista
                 lis_secret.append(numero)
+                #añadimos la misca cantidad de valores a nuestra lista para ver las pistas
                 list_pista.append(0)
             print("✅ Lista secreta creada correctamente.")
             break 
@@ -24,9 +30,9 @@ while True:
         print("⚠️  Solo se permiten números enteros.")
 
 while True:
-    instruccion = input("\nDesea continuar s/n: ")
+    instruccion = input("\nEscribe tu instrucción o 'adivinar': ")
 
-    if instruccion.strip().lower() == "s":
+    if instruccion.strip().lower() == "adivinar":
         intento = input("Escribe tu intento de lista (separada por comas): ")
         try:
             intento_lista = [int(x.strip()) for x in intento.split(",")]
