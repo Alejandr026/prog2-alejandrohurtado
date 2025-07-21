9ace84a1dfe132945ce7e1079d84d43894cc6a4d
AlE <alejandrohurtadorodas@gmail.com>
2025-07-21 17:26:29 -0400

    subiendo archivos todolis

--- ejercicosProg2Par2/sumaMatriz/sumaMatriz.py
+++ ejercicosProg2Par2/sumaMatriz/sumaMatriz.py
@@ -5,9 +5,15 @@ ventas = [
 [10, 10, 10, 10] # Producto 3 ...
 ]
 
+print(f"El numero mayor de la lista es:  ")
+
 acumulador_total = 0
 for i in range(len(ventas)):
     for j in range(len(ventas[i])):
         acumulador_total += ventas[i][j]
 
-print(acumulador_total)
+print("Suma total: ", acumulador_total)
+
+
+
+
